21d1b7006857afea4852954842ae2fd95e41106f
Roberto Ferro <roberto.ferro@ikdev.eu>
2018-11-27 19:00:27 +0100

    Test insert after backup

--- lib/backup.py
+++ lib/backup.py
@@ -1,26 +1,27 @@
 import tarfile
 import time
-from lib import logger
+from model import log
 
 
 class Backup:
-
-    def __init__(self, dir):
-        self.directory = dir
-
-    def run(self):
-        date = time.time()
-        log = logger.Logger()
-        
-        filename = f'backup-{date}.tar.gz'
-
-        try:
-            tar = tarfile.open(filename, 'w:gz')
-            tar.add(self.directory, filename)
-            tar.close()
-            log.directory_backup()
-            
-        except Exception as msg:
-            print(msg)
-            return False
-        return True
+	
+	def __init__(self, dir):
+		self.directory = dir
+	
+	def run(self):
+		date = time.time()
+		
+		filename = f'backup-{date}.tar.gz'
+		
+		try:
+			tar = tarfile.open(filename, 'w:gz')
+			tar.add(self.directory, filename)
+			tar.close()
+			
+			insert = log.Log().create(type='INFO', description='Test')
+			insert.save()
+		
+		except Exception as msg:
+			print(msg)
+			return False
+		return True
