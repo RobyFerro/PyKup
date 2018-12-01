3a4b32c7f4339c98676ded3cb18ac571d303fd85
Roberto Ferro <roberto.ferro@ikdev.eu>
2018-12-01 20:07:34 +0100

    Completed Postgres backup

--- lib/backup.py
+++ lib/backup.py
@@ -5,76 +5,59 @@ import datetime
 import json
 import subprocess
 from model import log
-from sys import platform
+import os
 
 
 class Backup:
-	
+
 	def __init__(self, dir):
 		self.directory = dir
-	
+
 	def run(self):
 		date = time.time()
-		
+
 		filename = f'backup-{date}.tar.gz'
-		
+
 		try:
 			tar = tarfile.open(filename, 'w:gz')
 			tar.add(self.directory, filename)
 			tar.close()
-			
+
 			insert = log.Log().create(
 				type='INFO',
 				description=f'Created .tar file of {self.directory} directory',
 				user=getpass.getuser(),
 				created_at=datetime.datetime.now()
 			)
-			
+
 			insert.save()
-		
+
 		except Exception as msg:
 			print(msg)
 			return False
 		return True
-	
+
 	@staticmethod
 	def dump():
-		
+
 		with open('config/database.json') as f:
 			db_config = json.load(f)
-		
+
 		if db_config['db_connection'] == 'pgsql':
-			
-			if platform == "linux" or platform == "linux2":
-				
-				Popen([
-					f'PGPASSWORD={db_config["db_password"]}',
-					f'pg_dump',
-					f'-h {db_config["db_host"]}',
-					f'-U {db_config["db_username"]}',
-					f'-d {db_config["db_name"]}',
-					f'-f test.dump'
-				])
-			
-			elif platform == 'win32':
-				
-				#
-				# TODO Not working
-				
-				psql_env = dict(PGPASSWORD='wsgen')
-				
-				cmd = f'pg_dump -h 127.0.0.1 -p 5432 -U wsgen -d ulisse_svil1'
-				
-				with open('test.dump', 'wb') as f:
-					popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, env=psql_env)
-					
-					for stdout_line in iter(popen.stdout.readline, ""):
-						f.write(stdout_line.encode('utf - 8'))
-						
-						popen.stdout.close()
-						popen.wait()
-		
+
+			os.putenv('PGPASSWORD', db_config['db_password'])
+
+			c = subprocess.Popen([
+				f'pg_dump',
+				f'--host={db_config["db_host"]}',
+				f'--user={db_config["db_username"]}',
+				f'--dbname={db_config["db_name"]}',
+				f'--file=./test.dump'
+			], stdout=subprocess.PIPE)
+
+			return c.communicate()[0]
+
 		elif db_config['db_connection'] == 'mysql':
 			print('To be completed')
-		
+
 		return True
--- pykup.py
+++ pykup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import sys, argparse
 from lib import backup
+from sys import platform
+
+if platform not in ['linux','linux2']:
+	print("Incorrect operative system")
+	exit(255)
 
 parser = argparse.ArgumentParser(description='PyBack WebApp backup utils')
 parser.add_argument('-d', action='store', dest='directory', help="Set a backup directory", type=str)
