432ee01d6b8f8fb260fbe56baff6cb0e4c69bfca
Roberto Ferro <roberto.ferro@ikdev.eu>
2019-01-09 11:15:01 +0100

    Resolved huge file upload on dropbox. Start max file age in scp integration

--- core/integrations/dropbox.py
+++ core/integrations/dropbox.py
@@ -8,17 +8,17 @@ from modules import env
 
 class DropboxIntegration:
 	dbx = None
-
+	
 	def __init__(self, destination, config_file):
 		self.destination = destination
 		self.config = env.get_config(config_file)['DROPBOX_INTEGRATION']
 		self.dbx = dropbox.Dropbox(self.config['ACCESS_TOKEN'])
-
+	
 	def upload(self, file):
-		f = open(f'/{self.destination}/{file}')
-		file_size = os.path.getsize(f'/{self.destination}/{file}')
+		f = open(file, 'rb')
+		file_size = os.path.getsize(file)
 		chunk_size = 4 * 1024 * 1024
-
+		
 		if file_size <= chunk_size:
 			with open(file, 'rb') as f:
 				self.dbx.files_upload(f.read(), f'/{self.destination}/{file}')
@@ -28,29 +28,29 @@ class DropboxIntegration:
 				session_id=upload_session_start_result.session_id,
 				offset=f.tell()
 			)
-
+			
 			commit = dropbox.files.CommitInfo(path=f'/{self.destination}/{file}')
-
+			
 			while f.tell() < file_size:
 				if (file_size - f.tell()) <= chunk_size:
 					self.dbx.files_upload_session_finish(f.read(chunk_size), cursor, commit)
 				else:
 					self.dbx.files_upload_session_append(f.read(chunk_size), cursor.session_id, cursor.offset)
 					cursor.offset = f.tell()
-
+		
 		self.check_backup_age()
-
+	
 	def check_backup_age(self):
-
+		
 		now = datetime.datetime.now()
-
+		
 		file_list = self.dbx.files_list_folder(path=f'/{self.destination}/exports')
-
+		
 		for file in file_list.entries:
-
+			
 			if abs(now - file.server_modified).days > int(self.config["DBX_MAX_BACKUP_AGE"]):
 				self.dbx.files_delete_v2(path=file.path_lower)
-
+	
 	def close_connection(self):
 		if self.dbx is not None:
 			self.dbx._session.close()
--- core/integrations/scp.py
+++ core/integrations/scp.py
@@ -1,6 +1,7 @@
 import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
+
 from modules import env
 
 
@@ -18,12 +19,17 @@ class SCPUpload:
 			self.config["SCP_USERNAME"],
 			self.config["SCP_PASSWORD"]
 		)
-			
-		
+	
 	def upload(self, file, remote_folder):
 		scp = SCPClient(self.ssh.get_transport())
 		scp.put(f'{file}', recursive=True, remote_path=remote_folder)
 		scp.close()
-		
+	
 	def close_scp_connection(self):
 		self.ssh.close()
+	
+	def check_backup_age(self, remote_folder):
+		sftp = self.ssh.open_sftp()
+		sftp.chdir(path=remote_folder)
+
+# TODO: Complete backup age
